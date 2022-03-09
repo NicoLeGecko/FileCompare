31462602a594891394c3b5f6526a68fb818bd9c6
NicoLeGecko <nicomarti13@gmail.com>
2022-03-10 00:47:40 +0100

    Implemented CLI

--- FileComparer.Cli/Program.cs
+++ FileComparer.Cli/Program.cs
@@ -4,6 +4,8 @@ using FileCompare;
 
 //Add validation that the entered arguments are valid
 
+
+
 var command = args[0];
 var directory = args[1];
 var dbPath = args[2];
@@ -13,12 +15,15 @@ var comparer = new FileComparer(dbPath);
 switch (command)
 {
     case "add":
+        Console.WriteLine("Adding to db");
         AddToDb(directory);
         break;
     case "search":
+        Console.WriteLine("Searching");
         Search(directory);
         break;
     default:
+        Console.WriteLine("Unknown command. Please try 'add' or 'search' ");
         break;
         // Inform user of unknown command
 }
@@ -27,6 +32,23 @@ void AddToDb(string directory) => comparer.AddToDb(directory);
 
 void Search(string directory)
 {
-    var duplicates = comparer.SearchForDuplicates(directory);
+    var duplicatesReport = comparer.SearchForDuplicates(directory);
+
     //Print files with duplicates
+
+    if (!duplicatesReport.Any())
+    {
+        Console.WriteLine("Found no duplicate of any known file previously added to the db.");
+    }
+
+    foreach (var report in duplicatesReport)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Found duplicate(s) of known file {report.Key} at:");
+
+        foreach (var duplicate in report.Value)
+        {
+            Console.WriteLine($"\t{duplicate}");
+        }
+    }
 }
